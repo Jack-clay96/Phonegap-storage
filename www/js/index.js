a9d09f9c0cfd7496a599f8b29b4fe78d24b4ceba
Jack-clay96 <claj2_14@uni.worc.ac.uk>
2017-10-19 16:13:06 +0100

    section 2

--- www/js/index.js
+++ www/js/index.js
@@ -1,16 +1,31 @@
-var phone = "nexus";
-var wallet = "black";
+var value = "nexus";
+var key = "phone";
+var value1 = "wallet";
+var key1 = "black";
 var glasses = "green";
 var drink = "coke";
 var food = "burger";
 
-        document.addEventListener("deviceready", onDeviceReady, false);
-		
+//JSON:
+var choc= {
+    "name" : "twix",
+    "weight" : "100g",
+    "carbs" : "20g"
+}
+
+
+
+window.localStorage.setItem( key, value);
+
+
+document.addEventListener("deviceready", onDeviceReady, false);
+//localStorage2();
+
+
 
 //$ is used in jquery. # = ID. $ is looking at the launched in HTML document. 	
 function updateDisplay() {
 	$("#launched").text("Application launched: " + launched_count);
-     
 }
 
 
@@ -18,17 +33,30 @@ function updateDisplay() {
 //
     function onDeviceReady() {
 	
-	updateDisplay();
-	    
+	updateDisplay();  
 	alert("device ready");
+    //localStorage2(); //best to call methods here. As it waits until phone is ready. (Cant test on webpage from here) 
     }
 
-    function localStorage()
+
+
+    function localStorage2() //Function name can't be something used for something else
     {
-        window.localStorage.setItem( phone, "nexus" );
-        var getPhone = window.localStorage.getItem(phone);
-        alert("localstorge");
+        window.localStorage.setItem( key, value );
+        var getPhone = window.localStorage.getItem(key);
+        alert("Phone: " + getPhone);
         
-        console.log("Phone: " + getPhone);
+         window.localStorage.setItem( key1, value1 );
+        var getPhone = window.localStorage.getItem(key1);
+        $("#key1").text("Wallet: ");
+        
+        var items = window.localStorage.length;
+        console.log("No. item stored: " + items)
         
     }
+
+function jsonString()
+{
+    var myJSON = JSON.stringify(choc); //turn the var choc into a string
+    console.log("my favorite chocolate: " + choc.name + " and  weight is: " + choc.weight);
+}
