9de9cf2b6416bcf0f46cb12a905e634c4f3354d7
Jack-clay96 <claj2_14@uni.worc.ac.uk>
2017-10-19 14:58:06 +0100

    update

--- www/js/index.js
+++ www/js/index.js
@@ -1,9 +1,16 @@
-document.addEventListener("deviceready", onDeviceReady, false);
+var phone = "nexus";
+var wallet = "black";
+var glasses = "green";
+var drink = "coke";
+var food = "burger";
+
+        document.addEventListener("deviceready", onDeviceReady, false);
 		
 
 //$ is used in jquery. # = ID. $ is looking at the launched in HTML document. 	
 function updateDisplay() {
 	$("#launched").text("Application launched: " + launched_count);
+     
 }
 
 
@@ -14,4 +21,12 @@ function updateDisplay() {
 	updateDisplay();
 	    
 	alert("device ready");
+    }
+
+    function localStorage()
+    {
+        window.localStorage.setItem( phone, "nexus" );
+        console.log(var getPhone = window.localStorage.getItem(phone));
+        alert("localstorge");
+        
     }
