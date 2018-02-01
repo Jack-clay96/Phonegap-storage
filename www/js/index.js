ccf767d939ba87b422ec05906950f121fb7340bf
Jack Clay <jclay@ipu.co.uk>
2018-02-01 13:31:47 +0000

    Login page

--- www/js/index.js
+++ www/js/index.js
@@ -1,25 +1,4 @@
-var value = "nexus";
-var key = "phone";
-var value1 = "wallet";
-var key1 = "black";
-//var glasses = "green";
-//var drink = "coke";
-//var food = "burger";
-
-//JSON:
-var choc= {
-    "name" : "twix",
-    "weight" : "100g",
-    "carbs" : "20g"
-}
-
-
-
-window.localStorage.setItem( key, value);
-
-
 document.addEventListener("deviceready", onDeviceReady, false);
-//localStorage2();
 
 
 
@@ -35,28 +14,6 @@ function updateDisplay() {
 	
 	updateDisplay();  
 	alert("device ready");
-    //localStorage2(); //best to call methods here. As it waits until phone is ready. (Cant test on webpage from here) 
     }
 
 
-
-    function localStorage2() //Function name can't be something used for something else
-    {
-        window.localStorage.setItem( key, value );
-        var getPhone = window.localStorage.getItem(key);
-        alert("Phone: " + getPhone);
-        
-         window.localStorage.setItem( key1, value1 );
-        var getPhone = window.localStorage.getItem(key1);
-        $("#key1").text("Wallet: ");
-		
-        var items = window.localStorage.length;
-        console.log("No. item stored: " + items);
-        
-    }
-
-function jsonString()
-{
-    var myJSON = JSON.stringify(choc); //turn the var choc into a string
-    console.log("my favorite chocolate: " + choc.name + " and  weight is: " + choc.weight);
-}
